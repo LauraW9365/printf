08d7516ad3855701f437497c4cdf90ab4457158b
LauraW9365 <laura.wafula@gmail.com>
2023-04-23 11:37:43 -0700

    printf

--- main.h
+++ main.h
@@ -1,13 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdarg.h>
 #include<stdio.h>
 #include <unistd.h>
 
 
 int _printf(const char *format, ...);
-int _putchar(char z);
+int _putchar(char c);
 
 
 #endif
